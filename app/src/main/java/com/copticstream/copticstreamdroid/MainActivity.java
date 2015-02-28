86633ba32fffa161a69ab236c176aae72d8f7868
mina <mina_gabriel84@hotmail.com>
2015-02-28 01:15:22 -0500

    Added New

---
+++ app/src/main/java/com/copticstream/copticstreamdroid/MainActivity.java
@@ -0,0 +1,28 @@
+package com.copticstream.copticstreamdroid;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import demo.StreamTest;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//        To Test the Live Stream -RTMP - last test Was 2/28/2015
+//        Intent intent = new Intent(this, StreamTest.class);
+//        startActivity(intent);
+
+        
+
+    }
+
+
+}
